8dde30a996d613d478bc1ea7263dd9c7555f3229
ruijia <pengrj2@gmail.com>
2025-02-21 12:34:10 +0800

    chore: slight edits to motor driver testing code

--- src/test/md_single.cpp
+++ src/test/md_single.cpp
@@ -1,5 +1,7 @@
 #include <SPI.h>
 #include <Arduino.h>
+// #include <Adafruit_GFX.h>
+// #include <Adafruit_SSD1306.h>
 #include <queue>
 #include <Adafruit_NeoPixel.h>
 
@@ -16,28 +18,31 @@ Adafruit_NeoPixel strip(led_count, led_pin, NEO_GRB + NEO_KHZ800);
 #define SPI_RW_BIT_MASK 0x4000  // Mask for SPI register read write indication bit
 
 // PINS
-#define CS_PIN 21 // 1
+// Arduino CLK = PIN 13
+// Arduino SDI (MISO) = PIN 12
+// Arduino SDO (MOSI) = PIN 11
+#define CS_PIN 1
 #define NSLEEP_PIN 9
 #define NFAULT_PIN 7
 #define IN2_PIN 5
 #define IN1_PIN 4
 #define DRVOFF_PIN 6
-#define MOSI_PIN 19 // 3 // TX
-#define MISO_PIN 20 // 0 // RX
-#define SCK_PIN 18 // 2
-#define SW1_PIN 14 // 26
-#define SW2_PIN 15 // 27
+#define MOSI_PIN 3 // TX
+#define MISO_PIN 0 // RX
+#define SCK_PIN 2
+#define SW1_PIN 26
+#define SW2_PIN 27
 #define ACCEL_DELAY_MICROS 2000
 #define ACCEL_DELAY_MILLIS 2
 
-const uint8_t IPROPI_pin = 29;
+const uint8_t IPROPI_pin = 8;   // @jx put the IPROPI pin number here, also make sure its a pin that can read analog
 const uint16_t A_IPROPI = 4750; // VQFN-HR package
 const uint32_t IPROPI_resistance = 680;
 const uint32_t ipropi_moving_average_size = 16384; // change this if uw, if its too big it wont fit
 
 uint8_t buffer[2];
 
-#define speedMaximum 1000000
+#define speedMaximum 8000000
 SPISettings MDSetting(speedMaximum, MSBFIRST, SPI_MODE1);
 
 class MotorDriver
@@ -48,7 +53,7 @@ public:
    * parameters of the device.
    * Register format |R/W|A5|A4|A3|A2|A1|A0|*|D7|D6|D5|D4|D3|D2|D1|D0|
    * Ax is address bit, Dx is data bits and R/W is read write bit.
-   * R/W bit should 0 for write, 1 for read
+   * For write R/W bit should 0.
    */
   void write8(uint8_t reg, uint8_t value)
   {
@@ -59,6 +64,8 @@ public:
     digitalWrite(CS_PIN, LOW);
     SPI.beginTransaction(MDSetting);
 
+    // SPI.transfer((uint8_t)((reg_value >> 8) & 0xFF));
+    // uint16_t received = SPI.transfer((uint8_t)(reg_value & 0xFF));
     uint16_t received16 = SPI.transfer16(reg_value & 0xFFFF);
     Serial.println((uint8_t)(received16), BIN);
     Serial.println((uint8_t)(received16 >> 8), BIN);
@@ -76,6 +83,8 @@ public:
     digitalWrite(CS_PIN, LOW);
     SPI.beginTransaction(MDSetting);
 
+    // SPI.transfer((uint8_t)((reg_value >> 8) & 0xFF));
+    // uint16_t recieved = SPI.transfer((uint8_t)(reg_value & 0xFF));
     uint16_t received16 = SPI.transfer16(reg_value & 0xFFFF);
     Serial.println((uint8_t)(received16), BIN);
     Serial.println((uint8_t)(received16 >> 8), BIN);
@@ -103,26 +112,19 @@ public:
     SPI.endTransaction();
   }
 
-  void accel(int pin, int max){
+  void accel(int pin){
     // Accelerate by increasing the PWM duty cycle
-    for (int i=0; i<=max; i++){
+    for (int i=0; i<=255; i++){
       analogWrite(pin, i);
       delay(ACCEL_DELAY_MILLIS);
     }
   }
 
-  void decel(int pin, int max){
+  void decel(int pin){
     // Decelerate by decreasing the PWM duty cycle
-    for (int i=max; i>=0; i--)
+    for (int i=255; i>=0; i--)
     {
       analogWrite(pin, i);
-      delay(ACCEL_DELAY_MILLIS);
-    }
-  }
-
-  void run(int pin, int speed){
-    for (int i=0; i<=255; i++){
-      analogWrite(pin, speed);
       delay(ACCEL_DELAY_MILLIS);
     }
   }
@@ -140,8 +142,6 @@ void setup()
   while(!Serial.available()){continue;}
   while(Serial.available()) Serial.read();
 
-  analogWriteFreq(5000);
-
   // Set pin modes
   Serial.println("Setting pin modes");
   pinMode(NSLEEP_PIN, OUTPUT);
@@ -153,6 +153,7 @@ void setup()
   pinMode(IN1_PIN, OUTPUT);
   // digitalWrite(IN1_PIN, 0);
   pinMode(NFAULT_PIN, INPUT_PULLUP);
+  digitalWrite(NFAULT_PIN, 0);
   pinMode(DRVOFF_PIN, OUTPUT);
   digitalWrite(DRVOFF_PIN, 0);
   pinMode(SW1_PIN, INPUT);
@@ -167,7 +168,6 @@ void setup()
   SPI.setTX(MOSI_PIN);
   SPI.setSCK(SCK_PIN);
   SPI.setCS(CS_PIN);
-  Serial.println("Initialising SPI");
   SPI.begin(); // initialize the SPI library
 
   // Set device configurations
@@ -179,49 +179,41 @@ void setup()
   // CONFIG4 Register
   // Bits 7-6: Set TOCP_SEL = 0b11 for lower TOCP
   // Bit 2: DRVOFF_SEL set to 1 by default
-  Serial.println("Setting TOCP");
+  // Serial.println("Setting TOCP");
   // motor_driver.write8(0x0D, 0b11000100);
   // motor_driver.spi_buffer(0b00001101, 0b11000100); 
-  delay(5);
+  // delay(5);
 
   // CONFIG2 Register
-  // Bit 2-0: Set S_ITRIP = 0b001 to set V_itrip to 1.18V or 0b100 for 1.98V
+  // Bit 2-0: Set S_ITRIP = 0b001 to set V_itrip to 1.18V
   Serial.println("Setting ITRIP");
   // motor_driver.write8(0x0B, 0b00000001);
-  motor_driver.spi_buffer(0b00001011, 0b00000100); 
-  delay(5);
-
-  // CONFIG1 Register
-  Serial.println("Setting config1");
-  // motor_driver.write8(0x0A, 0b00010000);
-  motor_driver.spi_buffer(0b00001010, 0b00010000); 
+  motor_driver.spi_buffer(0b00001011, 0b00000001); 
   delay(5);
 
   // CONFIG3 Register
   // Bits 1-0: Set S_MODE = 0b11 for PWM mode
   // Bits 4-2: Set S_SR = 0b111 for Slew Rate
-  // Bits 7-6: Set TOFF = 0b11 for max TOFF (50 micros) or 0b00 for min TOFF
+  // Bits 7-6: Set TOFF = 0b01 for TOFF
   Serial.println("Enter PWM mode");
   // motor_driver.write8(0x0C, 0b11011111);
   motor_driver.spi_buffer(0b00001100, 0b01011111); 
-  // motor_driver.spi_buffer(0b00001100, 0b01000011);
   delay(5);
 
-  // strip.begin();
-  // strip.show();
-  // strip.setBrightness(brightness);
-  // strip.setPixelColor(0, strip.Color(0, 0, 15));
-  // strip.show();
+  strip.begin();
+  strip.show();
+  strip.setBrightness(brightness);
+  strip.setPixelColor(0, strip.Color(0, 0, 15));
+  strip.show();
   delay(1000);
 }
 
 void loop()
 {
   float current, average;
-  Serial.print("looping");
-  Serial.println(counter);
-  // strip.setPixelColor(0, strip.Color(15, 15, 0));
-  // strip.show();
+  Serial.println("looping");
+  strip.setPixelColor(0, strip.Color(15, 15, 0));
+  strip.show();
   while(!Serial.available()) continue;
 
   // if(Serial.available()){
@@ -238,8 +230,8 @@ void loop()
   motor_driver.spi_buffer(0b01000001, 0b00000000);
   if(buffer[1]>0) {
     Serial.print("HELP FAULT");
-    // strip.setPixelColor(0, strip.Color(15, 0, 0));
-    // strip.show();
+    strip.setPixelColor(0, strip.Color(15, 0, 0));
+    strip.show();
     // while(1) continue;
   }
   Serial.println();
@@ -249,8 +241,8 @@ void loop()
   motor_driver.spi_buffer(0b01000010, 0b00000000);
   if(buffer[1]>16) {
     Serial.print("HELP STATUS");
-    // strip.setPixelColor(0, strip.Color(15, 0, 0));
-    // strip.show();
+    strip.setPixelColor(0, strip.Color(15, 0, 0));
+    strip.show();
     // while(1) continue;
   }
   Serial.println();
@@ -269,26 +261,30 @@ void loop()
   // motor_driver.read8(0b01001101, 0b00000000);
   motor_driver.spi_buffer(0b01001101, 0b00000000);
   Serial.println();
-
-  int maxspeed = 255;
-  // if(digitalRead(SW1_PIN)==HIGH){
-  //   motor_driver.accel(IN1_PIN, maxspeed);
-  //   motor_driver.run(IN1_PIN, maxspeed);
-  //   motor_driver.decel(IN1_PIN, maxspeed);
+  
+  // if(!ran){
+  // // analogWrite(IN2_PIN, 255);
+  // // delay(2);
+  // for (int i = 0; i <= 255; i++)
+  // {
+  //   analogWrite(IN2_PIN, i);
+  //   delayMicroseconds(2000);
+  //   // current = motor_driver.read_current();
+  //   // average = motor_driver.motor_current.add(current);
   // }
-  // else if(digitalRead(SW2_PIN)==HIGH){
-  //   motor_driver.accel(IN2_PIN, maxspeed);
-  //   motor_driver.run(IN2_PIN, maxspeed);
-  //   motor_driver.decel(IN2_PIN, maxspeed);
+  // delay(2);
+  // analogWrite(IN2_PIN, 0);
+  // delay(2);
+  // ran = true;
   // }
 
-  motor_driver.accel(IN1_PIN, maxspeed);
-  motor_driver.run(IN1_PIN, maxspeed);
-  motor_driver.decel(IN1_PIN, maxspeed);
-  delay(ACCEL_DELAY_MILLIS);
-  motor_driver.accel(IN2_PIN, maxspeed);
-  motor_driver.run(IN2_PIN, maxspeed);
-  motor_driver.decel(IN2_PIN, maxspeed);
-  delay(ACCEL_DELAY_MILLIS);
-  counter++;
+  if(digitalRead(SW1_PIN)==HIGH){
+    motor_driver.accel(IN1_PIN);
+    motor_driver.decel(IN1_PIN);
+  }
+  else if(digitalRead(SW2_PIN)==HIGH){
+    motor_driver.accel(IN2_PIN);
+    motor_driver.decel(IN2_PIN);
+  }
+
 }
