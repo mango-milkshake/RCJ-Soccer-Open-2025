6a05e91cdbc407e09ef4038a79266a634ea46e0a
ruijia <pengrj2@gmail.com>
2025-02-27 00:21:49 +0800

    feat: Motor Driver library

---
+++ src/lib/private/MotorDriver.h
@@ -0,0 +1,91 @@
+#ifndef MOTOR_DRIVER_H
+#define MOTOR_DRIVER_H
+
+#include <Arduino.h>
+#include <SPI.h>
+#define NUM_DRIVERS 4
+const int num_bytes = 2*NUM_DRIVERS + 2;
+SPISettings MDSetting(400000, MSBFIRST, SPI_MODE1);
+
+class MotorDriver {
+    public:
+        MotorDriver(uint8_t mosi, uint8_t miso, uint8_t sck, uint8_t cs, uint8_t nsleep, uint8_t drvoff) :
+            _mosi(mosi), _miso(miso), _sck(sck), _cs(cs), _nsleep(nsleep), _drvoff(drvoff) {
+            pinMode(_cs, OUTPUT);
+            pinMode(_nsleep, OUTPUT);
+            pinMode(_drvoff, OUTPUT);
+        }
+
+        uint8_t address[NUM_DRIVERS], data[NUM_DRIVERS];
+        uint8_t buffer[num_bytes];
+
+        void init(){
+            digitalWrite(_cs, HIGH);
+            digitalWrite(_nsleep, HIGH);
+            digitalWrite(_drvoff, LOW);
+
+            SPI.setRX(_miso);
+            SPI.setTX(_mosi);
+            SPI.setSCK(_sck);
+            SPI.setCS(_cs);
+            SPI.begin();
+        }
+
+        void daisychain(uint8_t addr[], uint8_t d[]){
+            digitalWrite(_cs, LOW);
+            SPI.beginTransaction(MDSetting);
+
+            buffer[0] = 0b10000000 + NUM_DRIVERS; // Header 1
+            buffer[1] = 0b10100000; // Header 2, change bit 5 to 1 for global CLR_FLT
+            for (int i=2; i<NUM_DRIVERS+2; i++){
+                // address bytes, last driver first
+                buffer[i] = addr[i-2];
+            }
+            for (int i=NUM_DRIVERS+2; i<num_bytes; i++){
+                buffer[i] = d[i-NUM_DRIVERS-2];
+            }
+
+            SPI.transfer(buffer, num_bytes);
+
+            for (int i=0; i<num_bytes; i++){
+                Serial.print(buffer[i], BIN);
+                Serial.print(" ");
+            }
+            Serial.println();
+
+            digitalWrite(_cs, HIGH);
+            SPI.endTransaction();
+        }
+
+        void spiComms(uint8_t addr, uint8_t d){
+            for (int i=0; i<NUM_DRIVERS; i++){
+                // start from last driver
+                address[i] = addr;
+                data[i] = d;
+            }
+            daisychain(address, data);
+        }
+
+        void setMode(){
+            spiComms(0b01001000, 0b10000000); // CLR_FLT comand
+
+            // CONFIG2 Register
+            // Bit 2-0: Set S_ITRIP = 0b001? to set V_itrip to 1.18V
+            spiComms(0b00001011, 0b00000100); // ITRIP
+
+            // CONFIG1 Register
+            spiComms(0b00001010, 0b00010000);
+
+            // CONFIG3 Register
+            // Bits 1-0: Set S_MODE = 0b11 for PWM mode
+            // Bits 4-2: Set S_SR = 0b111 for Slew Rate
+            // Bits 7-6: Set TOFF = 0b01
+            spiComms(0b00001100, 0b01011111);
+            digitalWrite(_drvoff, LOW);
+        }
+
+    private:
+        const uint8_t _mosi, _miso, _sck, _cs, _nsleep, _drvoff;
+};
+
+#endif
--- src/test/drive.cpp
+++ src/test/drive.cpp
@@ -3,6 +3,7 @@
 #include <Adafruit_NeoPixel.h>
 #include <Motor.h>
 #include <Drive.h>
+#include <MotorDriver.h>
 
 #define led_pin 16
 #define led_count 1
@@ -17,26 +18,17 @@ Adafruit_NeoPixel strip(led_count, led_pin, NEO_GRB + NEO_KHZ800);
 #define MOSI_PIN 3 // TX
 #define MISO_PIN 0 // RX
 #define SCK_PIN 2
-#define ACCEL_DELAY_MICROS 2000
-#define ACCEL_DELAY_MILLIS 2
 
-#define NUM_DRIVERS 4
-const int num_bytes = 2*NUM_DRIVERS + 2;
-uint8_t buffer[num_bytes];
-uint8_t address[NUM_DRIVERS];
-uint8_t data[NUM_DRIVERS];
+MotorDriver motor_driver(MOSI_PIN, MISO_PIN, SCK_PIN, CS_PIN, NSLEEP_PIN, DRVOFF_PIN);
 
-uint8_t IN1_pin[NUM_DRIVERS] = {7, 8, 10, 13};
-uint8_t IN2_pin[NUM_DRIVERS] = {6, 9, 11, 12};
+uint8_t IN1_pin[NUM_DRIVERS] = {6, 8, 11, 13};
+uint8_t IN2_pin[NUM_DRIVERS] = {7, 9, 10, 12};
 uint8_t IPROPI_pin[NUM_DRIVERS] = {26, 27, 28, 29}; // make sure pins can read analog
 
-#define speedMaximum 400000
-SPISettings MDSetting(speedMaximum, MSBFIRST, SPI_MODE1);
+uint8_t maxspeed = 20;
 
-uint8_t maxspeed = 50;
-
-Motor motorFL(IN1_pin[0], IN2_pin[0], maxspeed, 0.9);
-Motor motorFR(IN1_pin[3], IN2_pin[3], maxspeed, 0.9);
+Motor motorFL(IN1_pin[0], IN2_pin[0], maxspeed, 1.0);
+Motor motorFR(IN1_pin[3], IN2_pin[3], maxspeed, 1.0);
 Motor motorBL(IN1_pin[1], IN2_pin[1], maxspeed, 1.0);
 Motor motorBR(IN1_pin[2], IN2_pin[2], maxspeed, 1.0);
 
@@ -44,119 +36,21 @@ Drive bot(motorFR, motorBR, motorBL, motorFL);
 float speed = 1.0;
 float speedX, speedY, moveAngle;
 
-void daisychain(uint8_t addr[], uint8_t d[]){
-  digitalWrite(CS_PIN, LOW);
-  SPI.beginTransaction(MDSetting);
-
-  buffer[0] = 0b10000000 + NUM_DRIVERS; // Header 1
-  buffer[1] = 0b10100000; // Header 2, change bit 5 to 1 for global CLR_FLT
-  for (int i=2; i<NUM_DRIVERS+2; i++){
-    // address bytes, last driver first
-    buffer[i] = addr[i-2];
-  }
-  for (int i=NUM_DRIVERS+2; i<num_bytes; i++){
-    buffer[i] = d[i-NUM_DRIVERS-2];
-  }
-  
-  // for (int i=0; i<num_bytes; i++){
-  //   SPI.transfer(&buffer[i], 1);
-  // }
-  SPI.transfer(buffer, num_bytes);
-
-  for (int i=0; i<num_bytes; i++){
-    Serial.print(buffer[i], BIN);
-    Serial.print(" ");
-  }
-  Serial.println();
-
-  digitalWrite(CS_PIN, HIGH);
-  SPI.endTransaction();
-}
+int counter = 0;
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("started");
-  while(!Serial.available()) continue;
-  while(Serial.available()) Serial.read();
+  // Serial.println("started");
+  // while(!Serial.available()) continue;
+  // while(Serial.available()) Serial.read();
 
   analogWriteFreq(5000);
 
-  // Set pin modes
-  Serial.println("Setting pin modes");
-  pinMode(NSLEEP_PIN, OUTPUT);
-  digitalWrite(NSLEEP_PIN, 1);
-  pinMode(CS_PIN, OUTPUT);
-  digitalWrite(CS_PIN, 1);
-  // pinMode(NFAULT_PIN, INPUT_PULLUP);
-  pinMode(DRVOFF_PIN, OUTPUT);
-  digitalWrite(DRVOFF_PIN, 0);
-
-  for (int i=0; i<NUM_DRIVERS; i++){
-    pinMode(IN1_pin[i], OUTPUT);
-    pinMode(IN2_pin[i], OUTPUT);
-    Serial.print(IN1_pin[i]);
-    Serial.print(IN2_pin[i]);
-  }
-  Serial.println("Set pin modes");
-
-  delay(5);
-
-  // Set SPI
-  Serial.println("Setting SPI");
-  SPI.setRX(MISO_PIN);
-  SPI.setTX(MOSI_PIN);
-  SPI.setSCK(SCK_PIN);
-  SPI.setCS(CS_PIN);
-  SPI.begin(); // initialize the SPI library
-
-  // Set device configurations
-  Serial.println("Sending commands");
-  for (int i=0; i<NUM_DRIVERS; i++){
-    // start from last driver
-    address[i] = 0b01001000;
-    data[i] = 0b10000000; // Send CLR_FLT command
-  }
-  daisychain(address, data);
-  delay(5);
-
-  // CONFIG2 Register
-  // Bit 2-0: Set S_ITRIP = 0b001? to set V_itrip to 1.18V
-  Serial.println("Setting ITRIP");
-  for (int i=0; i<NUM_DRIVERS; i++){
-    // start from last driver
-    address[i] = 0b00001011;
-    data[i] = 0b00000100;
-  }
-  daisychain(address, data);
-  delay(5);
-
-  // CONFIG1 Register
-  Serial.println("Setting config1");
-  for (int i=0; i<NUM_DRIVERS; i++){
-    // start from last driver
-    address[i] = 0b00001010;
-    data[i] = 0b00010000;
-  }
-  daisychain(address, data);
-  delay(5);
-
-  // CONFIG3 Register
-  // Bits 1-0: Set S_MODE = 0b11 for PWM mode
-  // Bits 4-2: Set S_SR = 0b111 for Slew Rate
-  // Bits 7-6: Set TOFF = 0b11 for max TOFF (50 micros)
-  Serial.println("Enter PWM mode");
-  for (int i=0; i<NUM_DRIVERS; i++){
-    // start from last driver
-    address[i] = 0b00001100;
-    data[i] = 0b01011111; // 0b11011111;
-  }
-  daisychain(address, data);
-  digitalWrite(DRVOFF_PIN, 0);
-  delay(5);
+  motor_driver.init();
+  motor_driver.setMode();
 
   strip.begin();
-  strip.show();
   strip.setBrightness(brightness);
   strip.setPixelColor(0, strip.Color(0, 0, 15));
   strip.show();
@@ -168,20 +62,16 @@ void loop()
   Serial.println("looping");
   strip.setPixelColor(0, strip.Color(15, 15, 0));
   strip.show();
-  while(!Serial.available()) continue;
+  // while(!Serial.available()) continue;
 
-  Serial.print("FAULT: ");
-  for (int i=0; i<NUM_DRIVERS; i++){
-    // start from last driver
-    address[i] = 0b01000001;
-    data[i] = 0b00000000;
-  }
-  daisychain(address, data);
-  delay(5);
-  Serial.println();
+  // Serial.print("FAULT: ");
+  // motor_driver.spiComms(0b01000001, 0b00000000);
 
 // move straight
-//   bot.setDrive(speed, 0, 0); // speed, angle, rotationrate from pid calculations
+  // if(counter%500<250) bot.setDrive(speed, 0, 0);
+  // else bot.setDrive(0, 0, 0);
+  // counter++;
+  // if(counter>=500) counter = 0;
 
 // move in octagon
 //   for (int i=0; i<8; i++){
@@ -196,18 +86,18 @@ void loop()
 //   }
 
 // move in square
-//   for (int i=0; i<4; i++){
-//     bot.setDrive(speed, 90*i, 0);
-//     delay(300);
-//   }
+  // for (int i=0; i<4; i++){
+  //   bot.setDrive(speed, 90*i, 0);
+  //   delay(300);
+  // }
 
 // move in circle
-  for (int i=0; i<365; i++){
-    bot.setDrive(speed, i, 0);
-    delay(5);
-  }
+  // for (int i=0; i<365; i++){
+  //   bot.setDrive(speed, i, 0);
+  //   delay(5);
+  // }
 
 // testing
-//   bot.setDrive(speed, 135, 0); // speed, angle, rotationrate from pid calculations
+  bot.setDrive(speed, 0, 0);
 
 }
