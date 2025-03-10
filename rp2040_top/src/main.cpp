d0260377439a8d10fd37438461ef3956546099e4
ruijia <pengrj2@gmail.com>
2025-03-10 14:51:12 +0800

    feat: Integrate IMU reading on top plate
    - reading and taring yaw
    - rp2040 dual core
    - detecting when bot is picked up via IMU roll/pitch
    - fixed some issues

--- rp2040_top/src/main.cpp
+++ rp2040_top/src/main.cpp
@@ -3,6 +3,7 @@
 #include <Adafruit_NeoPixel.h>
 #include <RotatingCalipers.h>
 #include <CommonUtils.h>
+#include <IMU.h>
 
 #define PICO_LED 16
 #define PICO_LED_BRIGHTNESS 50
@@ -25,10 +26,29 @@ float calib[NUM_LIDARS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 
 #define NUM_POINTS 24
 Point coords[NUM_LIDARS], hull[NUM_LIDARS];
-float prev_heading;
+float prev_heading = 0;
 
 #define TX_PIN 0
 #define RX_PIN 1
+
+#define CS0_PIN 5
+#define MISO0_PIN 4 // RX
+#define MOSI0_PIN 3 // TX
+#define SCK0_PIN 2
+
+#define CS1_PIN 13
+#define MISO1_PIN 12 // RX
+#define MOSI1_PIN 15 // TX
+#define SCK1_PIN 14
+
+#define IMU_TARE_BUTTON 29
+IMU imu0(MOSI0_PIN, MISO0_PIN, SCK0_PIN, CS0_PIN, SPI);
+IMU imu1(MOSI1_PIN, MISO1_PIN, SCK1_PIN, CS1_PIN, SPI1);
+
+spin_lock_t *imuLock;
+float imu_angle = 0; // access only on core 0
+float shared_imu_angle = 0; // use mutex for accessing on both cores
+float yaw = 0; // access only on core 1
 
 void setup(){
     Serial.begin(115200);
@@ -51,6 +71,8 @@ void setup(){
     Serial1.setTX(TX_PIN);
     Serial1.begin(115200);
 
+    imuLock = spin_lock_instance(0);
+
     strip.begin();
     strip.setBrightness(STRIP_BRIGHTNESS);
     strip.setPixelColor(0, pico_led.Color(15, 15, 0));
@@ -60,6 +82,12 @@ void setup(){
     pico_led.setBrightness(PICO_LED_BRIGHTNESS);
     pico_led.setPixelColor(0, pico_led.Color(15, 15, 0));
     pico_led.show();
+}
+
+void setup1(){
+    pinMode(IMU_TARE_BUTTON, INPUT);
+    imu0.init();
+    // imu1.init();
 }
 
 void loop(){
@@ -76,14 +104,14 @@ void loop(){
     int hullSize = convexHull(coords, NUM_POINTS, hull);
     MinAreaRect rect = findMinAreaRect(hull, hullSize);
 
-    // Serial.print("dimensions");
-    // Serial.print(rect.width);
-    // Serial.print(" ");
-    // Serial.print(rect.height);
-    // Serial.print("\t");
+    Serial.print("dimensions");
+    Serial.print(rect.width);
+    Serial.print(" ");
+    Serial.print(rect.height);
+    Serial.print("\t");
 
-    // Serial.print("area: ");
-    // Serial.println(rect.area);
+    Serial.print("area: ");
+    Serial.println(rect.area);
 
     float heading = 0, basicAngle = 0;
 
@@ -105,9 +133,9 @@ void loop(){
         else if(rect.vector_width.y<0 && rect.vector_width.x>0) heading = 360 - heading;
     }
 
-    float other_heading = heading + (180 ? heading<180 : -180);
+    float other_heading = heading + (heading<180 ? 180 : -180);
     float diff1 = abs(prev_heading - heading), diff2 = abs(prev_heading - other_heading);
-    float final_heading = heading ? diff1 <= diff2 : other_heading;
+    float final_heading = diff1 <= diff2 ? heading : other_heading;
     int uart_heading = floor(final_heading * 128);
     prev_heading = final_heading;
     
@@ -127,6 +155,14 @@ void loop(){
     int rounded_coord_x = floor(cur_coords.x * 128);
     int rounded_coord_y = floor(cur_coords.y * 128);
 
+    if (!is_spin_locked(imuLock)) {  
+        uint32_t irq_state = spin_lock_blocking(imuLock);
+        imu_angle = shared_imu_angle;
+        spin_unlock(imuLock, irq_state);
+    }
+
+    int rounded_imu_angle = floor(abs(imu_angle) * 128);
+
     Serial1.write(1);
     Serial1.write(rounded_coord_x & 0xFF);
     Serial1.write((rounded_coord_x >> 8) & 0xFF);
@@ -134,5 +170,36 @@ void loop(){
     Serial1.write((rounded_coord_y >> 8) & 0xFF);
     Serial1.write(uart_heading & 0xFF);
     Serial1.write((uart_heading >> 8) & 0xFF);
+
+    if(copysign(1, imu_angle)==1) Serial1.write(1);
+    else Serial1.write((uint8_t)0);
+    Serial1.write(rounded_imu_angle & 0xFF);
+    Serial1.write((rounded_imu_angle >> 8) & 0xFF);
     
+}
+
+void loop1(){
+    if(digitalRead(IMU_TARE_BUTTON)==HIGH){
+        imu0.tareYaw();
+        // imu1.tareYaw();
+        // Serial.println("tared IMU0");
+    }
+    float angle0 = imu0.readYaw();
+    // float angle1 = imu1.readYaw();
+    // yaw = (angle0 + angle1) / 2;
+    yaw = angle0;
+    // if(yaw < 0) yaw += 360;
+    // if(yaw >= 360) yaw -= 360;
+    if (!is_spin_locked(imuLock)) {  
+        uint32_t irq_state = spin_lock_blocking(imuLock);
+        shared_imu_angle = yaw;
+        spin_unlock(imuLock, irq_state);
+        // Serial.print("IMU heading: ");
+        // Serial.println(yaw);
+    }
+
+    if(abs(imu0.roll)>=10 || abs(imu0.pitch)>=10){
+        pico_led.setPixelColor(0, pico_led.Color(0, 15, 0));
+        pico_led.show();
+    }
 }
