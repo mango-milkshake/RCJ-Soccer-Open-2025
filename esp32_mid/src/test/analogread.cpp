2e6e68e8c594f5deb92f290eb3d96115096574de
ruijia <pengrj2@gmail.com>
2025-03-19 00:29:08 +0800

    feat: read battery voltage on ESP32

--- esp32_mid/src/test/analogread.cpp
+++ esp32_mid/src/test/analogread.cpp
@@ -5,13 +5,16 @@ void setup() {
     Serial.begin(115200);
     Serial.println("started");
     pinMode(VOLTAGE_PIN, INPUT);
+    analogSetAttenuation(ADC_11db);
 }
 
 void loop() {
     // Serial.println("running");
     float value = analogRead(VOLTAGE_PIN);
-    value = value * 13.3 / 3.3;
+    float voltage = (value+1) * 13.3 / 4096;
+    voltage = voltage / 12.3 * 11.8;
     Serial.print(value);
     Serial.print("\t");
+    Serial.println(voltage);
     delay(10);
 }
