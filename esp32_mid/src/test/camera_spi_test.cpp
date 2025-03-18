5acf09e1e7934714fd839877c4c8768d8c7b5830
ruijia <pengrj2@gmail.com>
2025-03-18 14:27:30 +0800

    feat: camera SPI comms (dont work)

--- esp32_mid/src/test/camera_spi_test.cpp
+++ esp32_mid/src/test/camera_spi_test.cpp
@@ -5,10 +5,9 @@
 #define MISO_PIN 6
 #define SCK_PIN 5
 #define CS_PIN 4
-SPIClass spi = SPIClass();
 
-#define speedMaximum 100000
-SPISettings CamSetting(speedMaximum, MSBFIRST, SPI_MODE0);
+#define speedMaximum 400000
+SPISettings CamSetting(speedMaximum, MSBFIRST, SPI_MODE2);
 
 #define DATA_LEN 9
 byte buffer[DATA_LEN];
@@ -19,36 +18,35 @@ void setup(){
     while(Serial.available()) Serial.read();
     Serial.println("started");
 
-    analogWriteFrequency(5000);
-
-    spi.begin(SCK_PIN, MISO_PIN, MOSI_PIN, CS_PIN);
+    SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, CS_PIN);
     pinMode(CS_PIN, OUTPUT);
     digitalWrite(CS_PIN, HIGH);
 }
 
 void loop(){
-    digitalWrite(CS_PIN, LOW);
-    spi.beginTransaction(CamSetting);
-    spi.transferBytes(NULL, buffer, DATA_LEN);
-    digitalWrite(CS_PIN, HIGH);
-    spi.endTransaction();
 
-    // for (auto i : buffer){
-    //     Serial.print(i);
-    //     Serial.print(" ");
-    // }
+    SPI.beginTransaction(CamSetting);
+    digitalWrite(CS_PIN, LOW);
+    SPI.transferBytes(NULL, buffer, DATA_LEN);
+    digitalWrite(CS_PIN, HIGH);
+    SPI.endTransaction();
+
+    for (auto i : buffer){
+        Serial.print(i);
+        Serial.print(" ");
+    }
 
     float ball_angle = (float)(buffer[1] + (buffer[2]<<8)) / 128;
     float ball_dist = (float)(buffer[3] + (buffer[4]<<8)) / 128;
     float goal_angle = (float)buffer[5] + (buffer[6]<<8) / 128;
     float goal_dist = (float)buffer[7] + (buffer[8]<<8) / 128;
-    Serial.print(ball_angle, 3);
-    Serial.print("\t");
-    Serial.print(ball_dist, 3);
-    Serial.print("\t");
-    Serial.print(goal_angle, 3);
-    Serial.print("\t");
-    Serial.print(goal_dist, 3);
+    // Serial.print(ball_angle, 3);
+    // Serial.print("\t");
+    // Serial.print(ball_dist, 3);
+    // Serial.print("\t");
+    // Serial.print(goal_angle, 3);
+    // Serial.print("\t");
+    // Serial.print(goal_dist, 3);
 
     Serial.println();
     delay(10);
