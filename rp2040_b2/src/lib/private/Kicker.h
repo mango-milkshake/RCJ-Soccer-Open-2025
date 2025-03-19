0fbd7c2dc39b1663c4f65bce894fcdc370813140
ruijia <pengrj2@gmail.com>
2025-03-20 00:50:24 +0800

    feat: Kicker logic test through bottom plate

---
+++ rp2040_b2/src/lib/private/Kicker.h
@@ -0,0 +1,29 @@
+#ifndef KICKER_H
+#define KICKER_H
+
+#include <Arduino.h>
+#define KICKER_DELAY 60
+
+class Kicker {
+    public:
+        Kicker(int pin) :
+            _pin(pin) { // constructor
+            pinMode(_pin, OUTPUT);
+            digitalWrite(_pin, LOW);
+            _lastKick = 0;
+        };
+        void kick() {
+            float _curTime = millis();
+            if(_curTime - _lastKick < 2000) return;
+            digitalWrite(_pin, HIGH);
+            delay(KICKER_DELAY);
+            digitalWrite(_pin, LOW);
+            _lastKick = millis();
+        };
+
+    private:
+        const int _pin;
+        float _lastKick;
+};
+
+#endif
