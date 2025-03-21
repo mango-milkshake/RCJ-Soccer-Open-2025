0af05698d8f42a19357be4adebb6a2ba1b15b550
ruijia <pengrj2@gmail.com>
2025-03-21 10:56:10 +0800

    feat: start bottom plate 2nd rp2040 zero main code

--- rp2040_b2/src/main.cpp
+++ rp2040_b2/src/main.cpp
@@ -1,10 +1,52 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_NeoPixel.h>
+#define DEBUGGING
 
-void setup() {
-    Serial.begin(9600);
+#define led_pin 16
+#define led_count 1
+#define brightness 50
+Adafruit_NeoPixel strip(led_count, led_pin, NEO_GRB + NEO_KHZ800);
+
+#define SDA_PIN 6
+#define SCL_PIN 7
+#define I2C_DATA_LEN 16
+#define ADDR 0x08
+byte sendBuffer[I2C_DATA_LEN];
+// 0: start byte = 1
+// 1-5: top cam
+// 6-10: ballcap lidar
+// 11: tofsense lidar gate
+// 12-15: line sensors
+
+void send(){
+    #ifdef DEBUGGING
+    for (auto i : sendBuffer){
+        Serial.print(String(i) + " ");
+    }
+    Serial.println();
+    #endif
+
+    Wire1.write(sendBuffer, I2C_DATA_LEN);
 }
 
-void loop() {
-    Serial.println("Hello World!");
-    delay(10);
+void setup(){
+    Serial.begin(115200);
+
+    Wire1.setSDA(SDA_PIN);
+    Wire1.setSCL(SCL_PIN);
+    Wire1.setClock(400000);
+    Wire1.begin(ADDR);
+
+    strip.begin();
+    strip.setBrightness(brightness);
+    strip.setPixelColor(0, strip.Color(15, 15, 0));
+    strip.show();
+}
+
+void loop(){
+    strip.setPixelColor(0, strip.Color(0, 15, 15));
+    strip.show();
+    
+    Wire1.onRequest(send);
 }
