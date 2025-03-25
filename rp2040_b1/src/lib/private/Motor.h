293b4ea7ca45dcc5bbdace05147a955ebbf130f8
ruijia <pengrj2@gmail.com>
2025-03-25 18:44:43 +0800

    feat: Add PWM offset

--- rp2040_b1/src/lib/private/Motor.h
+++ rp2040_b1/src/lib/private/Motor.h
@@ -16,17 +16,14 @@ class Motor {
         }
 
         void setSpeed(float speed) {
-            // _speed = constrain(speed, max(-1, _lastSpeed-0.1), min(_lastSpeed+0.1, 1));
-            // _lastSpeed = _speed;
-            // _speed = _speed * _multiplier;
             _speed = speed * _multiplier;
             _speed = constrain(_speed, -1, 1);
             if (_speed >= 0) {
                 analogWrite(_pin2, 0);
-                analogWrite(_pin1, abs(_speed)*_maxspeed);
+                analogWrite(_pin1, abs(_speed)*_maxspeed + 10);
             } else {
                 analogWrite(_pin1, 0);
-                analogWrite(_pin2, abs(_speed)*_maxspeed);
+                analogWrite(_pin2, abs(_speed)*_maxspeed + 10);
             }
             // Serial.print(_pin1);
             // Serial.print(": ");
