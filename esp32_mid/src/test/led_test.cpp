096fe411eeb254f91204c1529d88e35beb94984f
ruijia <pengrj2@gmail.com>
2025-03-26 23:17:43 +0800

    feat: individual testing code for middle plate NeoPixels

---
+++ esp32_mid/src/test/led_test.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN 18
+#define LED_COUNT 12
+#define LED_BRIGHTNESS 100
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+#define ESP_LED 21
+#define ESP_BRIGHTNESS 50
+Adafruit_NeoPixel esp_led(1, ESP_LED, NEO_GRB + NEO_KHZ800);
+
+void setup(){
+    strip.begin();
+    strip.setBrightness(LED_BRIGHTNESS);
+    strip.show();
+
+    esp_led.begin();
+    esp_led.setBrightness(ESP_BRIGHTNESS);
+    esp_led.show();
+}
+
+void loop(){
+    esp_led.setPixelColor(0, esp_led.Color(0, 0, 15));
+    esp_led.show();
+    for (int i=0; i<LED_COUNT; i++){
+        strip.setPixelColor(i, strip.Color(15, 15, 15));
+    }
+    strip.show();
+}
