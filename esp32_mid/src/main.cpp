0897891c72e0385411a179401fd73c0d285ea7f1
ruijia <pengrj2@gmail.com>
2025-03-26 23:35:01 +0800

    feat: tune rotation pid slightly

--- esp32_mid/src/main.cpp
+++ esp32_mid/src/main.cpp
@@ -58,7 +58,7 @@ byte uartBufferPico[PICO_SERIAL_DATA_LEN];
 byte uartBufferCam[CAM_SERIAL_DATA_LEN];
 
 // PID
-PID pid_rotate(2, 0, 0, 1000);
+PID pid_rotate(1, 0, 0, 1000);
 PID pid_x(6, 0, 0, 5000);
 PID pid_y(6, 0, 0, 5000);
 
