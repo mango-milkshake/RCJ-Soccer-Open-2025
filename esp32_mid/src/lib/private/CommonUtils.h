8adafb06946c4367da1e2a50199af6566142473b
ruijia <pengrj2@gmail.com>
2025-03-31 22:43:29 +0800

    feat: update middle plate commonutils library

--- esp32_mid/src/lib/private/CommonUtils.h
+++ esp32_mid/src/lib/private/CommonUtils.h
@@ -9,7 +9,8 @@
 #define DEG(x) ((x) * 180.0f / (float) PI)
 
 // Limit angle to 0 to 360 degrees
-#define LIM_ANGLE(angle) (angle > 0 ? fmod(angle, 360) : fmod(angle, 360) + 360)
+#define LIM_ANGLE_360(angle) (angle > 0 ? fmod(angle, 360) : fmod(angle, 360) + 360)
+#define LIM_ANGLE_180(angle) if(angle < -180) angle += 360; if(angle > 180) angle -= 360;
 
 #define DELTA_ANGLE(a, b) (abs(fmod((a - b + 180), 360)) - 180)
 #define ANGLE_360_TO_180(angle) (angle > 180 ? angle - 360 : angle)
