674f98e93e65ac147ec9d8701bb0407b6c154707
ruijia <pengrj2@gmail.com>
2025-05-26 19:52:24 +0800

    feat: add new front cam code
    - with modified comms (remove ball velocity calculation)
    - also update camera ball thresholds

--- cameras/top_cam_new.py
+++ cameras/top_cam_new.py
@@ -39,7 +39,7 @@ sensor.set_windowing(window_x - int((window_width/2)), window_y - int((window_wi
 centre_x = 130
 centre_y = 111
 
-thresh_ball = (54, 84, 12, 75, 23, 57)
+thresh_ball = (41, 79, 12, 58, 8, 46)
 thresh_yellow_goal = (45, 100, -25, 15, 24, 72) # old
 thresh_blue_goal = (46, 58, -27, -7, -34, -17) # old
 
