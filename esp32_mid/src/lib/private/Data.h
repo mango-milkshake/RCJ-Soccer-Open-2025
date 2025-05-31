35e8740aff9070b0704def446e5582691b488309
ruijia <pengrj2@gmail.com>
2025-05-31 21:48:01 +0800

    feat: new Data library to organise data into categories
    Categories:
    - Localisation: robot coordinates and line status
    - Ball: ball related info - coordinates, velocity, last seen time, last seen position, ballcap, etc
    - Switches: turning off (both middle and top plate through comms) and other switches
    - movement: stores output target coordinates and heading
    - state: keeps track of strategies and defender/attacker state
    
    States:
    - Strategy types: no ball case, ball track, ballcap/scoring
    - Specific strategies eg move to point, oscillate about point, balltrack, scoring, defend, attack modes eg ball hide
    - defender/attacker state
    - current and last strategy type
    - current specific strategy index (used for tracking index inside a set of strategies to swap between in main code)
    - time that strategy was last changed

---
+++ esp32_mid/src/lib/private/Data.h
@@ -0,0 +1,52 @@
+#ifndef DATA_H
+#define DATA_H
+
+struct Localisation{
+    float x = 0, y = 0, heading = 0;
+    bool onLine = false;
+    uint8_t line_status = 0;
+} self;
+
+struct Ball{
+    float angle = 0, dist = 0, vx = 0, vy = 0;
+    float relative_x = 0, relative_y = 0;
+    float absolute_x = 0, absolute_y = 0;
+    float last_x = 0, last_y = 0;
+    bool noBall = false, ballCap = false;
+    int lastBallCap = 0, lastNoBallCap = 0, lastSeenBall = 0;
+} ball;
+
+struct Switches{
+    float topOff = true, turnOff = false;
+} switches;
+
+struct Movement{
+    float x = 0, y = 0, rotation = 0;
+} move;
+
+struct State{
+    bool isDefender = true;
+    int lastChange = 0;
+    int lastType = 0, curType = 0;
+    int curStratIdx = 0;
+
+    enum StratType{
+        NO_BALL = 0,
+        BALL_TRACK = 1,
+        SCORE = 2
+    };
+
+    enum Strategies{
+        NONE = 0,
+        MOVE_TO_POINT = 1,
+        OSCILLATE_ABOUT_POINT = 2,
+        NO_DRIBBLER_BALL_TRACK = 3,
+        NO_DRIBBLER_SCORE = 4,
+        DRIBBLER_BALL_TRACK = 5,
+        DRIBBLER_SCORE = 6,
+        DEFEND = 7,
+        ATTACK_MODE1 = 8
+    } strategies;
+} state;
+
+#endif
