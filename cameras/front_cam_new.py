45b0cc50b5ff53ff9e362052044d040cbd98c26c
shreddedpaper <123787892+shreddedpaper@users.noreply.github.com>
2025-06-18 18:05:24 +0800

    Updated front_cam_new.py fitted for new mount
    markers on image shows u center of robot(to algin ruler) shld comment out when calibrating

--- cameras/front_cam_new.py
+++ cameras/front_cam_new.py
@@ -1,68 +1,107 @@
-import sensor, time, math, pyb
+import sensor
+import time
+import math
 from ulab import numpy as np
-from pyb import UART
 
-def sendUART(x, sign):
-    if sign:
-        if x<0:
-            uart.writechar(0)
-        else:
-            uart.writechar(1)
-    multiplied_x = round((abs(x)) * 128)
-    uart.writechar(multiplied_x & 0xFF)
-    uart.writechar((multiplied_x >> 8) & 0xFF)
-
-threshold = (6, 92, 42, 84, 42, 65) # orange ball
+threshold = (20, 87, 12, 80, 26, 74) # orange ball
 x = 0
 y = 0
 
 sensor.reset()  # Reset and initialize the sensor.
 sensor.set_pixformat(sensor.RGB565)  # Set pixel format to RGB565 (or GRAYSCALE)
 sensor.set_framesize(sensor.QVGA)  # Set frame size to QVGA (320x240)
-
+sensor.skip_frames(time=200)  # Wait for settings take effect.
 clock = time.clock()  # Create a clock object to track the FPS.
+sensor.set_auto_gain(False)  # must be turned off for color tracking
 sensor.set_auto_whitebal(False)
 sensor.set_auto_exposure(False, exposure_us=6000)  # Disable auto exposure
-sensor.set_auto_gain(False) # must be turned off for colour tracking
-sensor.skip_frames(time=500)  # Wait for settings take effect.
 
-led2 = pyb.LED(2)
-no_ball = False
-uart = UART(3, 115200)
+
+
+prevCoords = [0,0]
+prevTime = 0
+v = 0
+vel_values_x = []
+vel_values_y = []
+vel_values_length = 10
+vel_values_x = [0] * vel_values_length
+vel_values_y = [0] * vel_values_length
 
 while True:
     clock.tick()  # Update the FPS clock.
-    led2.on()
     img = sensor.snapshot()  # Take a picture and return the image.
+    img.lens_corr(strength=1.48, zoom=1.0)
     blobs = img.find_blobs([threshold], area_threshold = 150, merge=True)
-    if len(blobs)>0:
+    img.draw_cross(120, 120, color=(0, 255, 0));
+    img.draw_cross(310, 120, color=(0, 255, 0));
+    for blob in blobs:
         ball = max(blobs, key=lambda b: b.area())
-        no_ball = False
         img.draw_rectangle(ball.rect(), color=(0,255,0))
         img.draw_cross(ball.cx(), ball.cy(), color=(0,0,255))
+
         print(ball.cx(), ball.cy())
         x = ball.cx()
         y = ball.cy()
-        x2 = 2.34678e-7* x**3 - 6.93269e-7* x**2 * y + 0.0000184518 * x**2 + 4.94946e-9 *x * y**2 + 0.000340658*x*y - 0.0405575*x + 1.26915e-7 * y**3 - 0.0000321536*y**2 - 0.0783178*y + 9.3546
-        y2 = -2.17938e-6 * x**3 - 3.57085e-8 * x**2 * y + 0.00129718 * x**2 - 3.14209e-7 * x * y**2 + 0.0000760482 * x * y - 0.27871*x + 6.65652e-7 * y**3 - 0.0001672 * y**2 + 0.00840698*y + 33.1509
 
+        x2 = (
+            1.46886e-20   * x**3
+            - 3.0779e-6   * x**2 * y
+            + 0.000369348 * x**2
+            - 3.91234e-21 * x * y**2
+            + 0.00158733  * x * y
+            - 0.190479    * x
+            - 1.20828e-6  * y**3
+            + 0.000434979 * y**2
+            - 0.277897    * y
+            + 29.1719
+        )
+
+        y2 = (
+            -7.9634e-6    * x**3
+            + 2.7854e-20   * x**2 * y
+            + 0.00535085  * x**2
+            + 9.05357e-8   * x * y**2
+            - 0.0000217286* x * y
+            - 1.21978     * x
+            - 4.6143e-21  * y**3
+            + 0.0000474553* y**2
+            - 0.0113893   * y
+            + 110.458
+        )
         dist = (x2**2 + y2**2)**0.5
-
         print("dist = ", dist)
         #print("x = ", x) #image coordinates
         #print("y = ", y)
         print("x2 = ", x2) #actual coordinates
         print("y2 = ", y2)
-    else:
-        no_ball = True
 
-    uart.writechar(5)
-    if(no_ball==False):
-        sendUART(x2, True)
-        sendUART(y2, False)
-    else:
-        for i in range(5):
-            uart.writechar(0)
-    uart.sendbreak()
+        #update values
+        coords = [x2,y2]
+        t = time.ticks_ms()
 
-    print("fps", clock.fps())
+        dx = coords[0]-prevCoords[0]
+        dy = coords[1]-prevCoords[1]
+        #print("dx = ", dx)
+        #print("dy = ", dy)
+
+        dt = (t-prevTime)/1000
+        #print ("dt =", dt) #~25ms
+
+        #calculate velocity
+        if (dt != 0):
+            vx = dx/dt #cm/s
+            vy = dy/dt
+
+
+        #find average velocity
+        for i in range(vel_values_length-1):
+            vel_values_x[i] = vel_values_x[i+1]
+            vel_values_y[i] = vel_values_y[i+1]
+        vel_values_x[-1] = vx
+        vel_values_y[-1] = vy
+        vx = sum(vel_values_x)/len(vel_values_x)
+        vy = sum(vel_values_y)/len(vel_values_y)
+        #print ("vx =", vx)
+        #print ("vy =", vy)
+
+        #log previous values
