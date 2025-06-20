aa50d5360cbe311afca919becdbbcb47f353d82d
ruijia <pengrj2@gmail.com>
2025-06-20 16:02:37 +0800

    feat: change dribbler speed output in Data lib

--- esp32_mid/src/lib/private/Data.h
+++ esp32_mid/src/lib/private/Data.h
@@ -30,9 +30,7 @@ struct Times{
 struct Movement{
     float x = 0, y = 0, rotation = 0;
     bool kick = false;
-    float dribblerSpeed = 0;
-    int min_translation = -80, max_translation = 80;
-    int min_rotation = -50, max_rotation = 50;
+    int dribbler_maxspeed = 150, dribblerSpeed = 0;
 } move;
 
 struct State{
