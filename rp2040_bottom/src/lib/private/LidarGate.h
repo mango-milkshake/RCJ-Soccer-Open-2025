9fed3d0516d3c5e95141c2d2e52bf19b2b301b14
ruijia <pengrj2@gmail.com>
2025-06-26 12:28:10 +0800

    fix: lidar gate threshold

--- rp2040_bottom/src/lib/private/LidarGate.h
+++ rp2040_bottom/src/lib/private/LidarGate.h
@@ -4,7 +4,7 @@
 #include <Arduino.h>
 #include "../public/analog_iic.h"
 #include "../public/tofsense_f_iic.h"
-#define LIDAR_BALLCAP_THRESH 0.06f
+#define LIDAR_BALLCAP_THRESH 0.04f
 
 class LidarGate{
     public:
