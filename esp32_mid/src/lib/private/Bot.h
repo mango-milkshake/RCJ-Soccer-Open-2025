abd7d1f5b2f160eb010cd78cbdf8c4b06906c746
ruijia <pengrj2@gmail.com>
2025-07-11 17:06:26 +0800

    feat: new ball hide

--- esp32_mid/src/lib/private/Bot.h
+++ esp32_mid/src/lib/private/Bot.h
@@ -618,9 +618,10 @@ class Bot{
                 //move to target
                 move.x = ballhide.mid_x;
                 move.y = 0.6;
-                    if(within(self.x, ballhide.mid_x, 0.05) && within(self.y, 0.6, 0.05)){
-                        state.ballhide_stage = 2;
-                    }
+                move.rotation = 0;
+                if(within(self.x, ballhide.mid_x, 0.05) && within(self.y, 0.6, 0.05)){
+                    state.ballhide_stage = 2;
+                }
                 break;
             case 2:
                 if(espnowDataRecv.bh_stage == 2 || espnowDataRecv.isPresent != 2){ //check if follower is in front
@@ -636,10 +637,13 @@ class Bot{
             case 3:
                 if(espnowDataRecv.bh_stage == 3 || espnowDataRecv.isPresent != 2){ //score after checking that goal is open
                     state.ready_to_shoot = true;
+                    move.dont_move = true;
                 }
+                else move.dont_move = true;
                 break;
             default:
                 state.ballhide_stage = 1;
+                move.dont_move = true;
                 break;
             }
         }   
@@ -648,17 +652,19 @@ class Bot{
             switch(state.ballhide_stage){
             case 1:
                 //move beside target
-                move.x = ballhide.mid_x - 0.3;
+                move.x = self.x;
                 move.y = espnowDataRecv.ypos + 0.25;
-                if(within(self.y, espnowDataRecv.ypos + 0.25, 0.05) && within(self.x, ballhide.mid_x - 0.3, 0.05)){ 
+                move.rotation = 180;
+                if(within(self.y, espnowDataRecv.ypos + 0.25, 0.05)){ 
                     //move to target (in front of leader)
                     move.x = ballhide.mid_x;
                     move.y = espnowDataRecv.ypos + 0.25;
                     move.rotation = 180;
                 }
-                else if(within(self.y, espnowDataRecv.ypos + 0.25, 0.05) && within(self.x, ballhide.mid_x, 0.05)){
+                if(within(self.y, espnowDataRecv.ypos + 0.25, 0.05) && within(self.x, ballhide.mid_x, 0.05)){
                     //signal that ready to ballhide
                     state.ballhide_stage = 2;
+                    move.dont_move = true;
                 }
                 break;
             case 2:
@@ -666,19 +672,25 @@ class Bot{
                     //check if close to goal, then move to side to open up goal for leader
                     move.x = ballhide.mid_x - 0.5;
                     move.y = ballhide.mid_y;
-                    if(within(self.x, ballhide.mid_x - 0.5, 0.05))
-                        //signal that goal is open
-                        state.ballhide_stage = 3;
+                    move.rotation = 180;
+                }
+                else if(within(self.x, ballhide.mid_x - 0.5, 0.05)){
+                    //signal that goal is open
+                    state.ballhide_stage = 3;
+                    move.dont_move = true;
                 }
                 else if(espnowDataRecv.bh_stage == 2){
                     //move up field
                     moveAlongY(ballhide.mid_x, ballhide.mid_y, 0);
                 }
+                else move.dont_move = true;
                 break;
             case 3: //do nothing
+                move.dont_move = true;
                 break;
             default:
                 state.ballhide_stage = 1;
+                move.dont_move = true;
             }
         }
 
