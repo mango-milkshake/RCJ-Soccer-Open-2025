1ef31b24b49686df63f814e3c3bce9bf6baf9254
ruijia <pengrj2@gmail.com>
2025-07-28 02:23:51 +0800

    Revert "chore: file rename"
    This reverts commit 1557ab6bf963a832dff049899a5ba84a9a4a7cdc.

--- rp2040_lower/src/main.cpp
+++ rp2040_lower/src/main.cpp
@@ -4,22 +4,12 @@
 #include <Motor.h>
 #include <MotorDriver.h>
 #include <Drive.h>
-#include <UARTComms.h>
 
-#define DEBUGGING
-#ifdef DEBUGGING
-#define DEBUG(x) Serial.println(String(#x) + String(": ") + String(x) + String('\r')); 
-#else
-#define DEBUG(x) 123;
-#endif
-
-#define CONT_CLR
-
-#define TX_PIN 4
-#define RX_PIN 5
+#define SDA_PIN 4
+#define SCL_PIN 5
 #define DATA_LEN 7
-byte buffer[DATA_LEN];
-UARTComms espUART(TX_PIN, RX_PIN, buffer, DATA_LEN, Serial2);
+#define ADDR 0x09
+byte buffer[DATA_LEN+1];
 
 #define led_pin 16
 #define led_count 1
@@ -31,6 +21,7 @@ float lastLED = 0;
 
 #define CS_PIN 1
 #define NSLEEP_PIN 14
+// #define NFAULT_PIN 7
 #define DRVOFF_PIN 15
 #define MOSI_PIN 3 // TX
 #define MISO_PIN 0 // RX
@@ -48,7 +39,7 @@ uint8_t IN2_pin[NUM_DRIVERS] = {6, 8, 10, 12};
 uint8_t NFAULT_pin[NUM_DRIVERS] = {26, 27, 28, 29};
 #endif
 
-uint8_t maxspeed = 210; // 130
+uint8_t maxspeed = 100;
 
 Motor motorFL(IN1_pin[0], IN2_pin[0], NFAULT_pin[0], maxspeed, 1.0);
 Motor motorFR(IN1_pin[3], IN2_pin[3], NFAULT_pin[3], maxspeed, 1.0);
@@ -56,57 +47,81 @@ Motor motorBL(IN1_pin[1], IN2_pin[1], NFAULT_pin[1], maxspeed, 1.0);
 Motor motorBR(IN1_pin[2], IN2_pin[2], NFAULT_pin[2], maxspeed, 1.0);
 float lastFault = 0;
 
-Drive bot(motorFR, motorBR, motorBL, motorFL, maxspeed);
-int speedX = 0, speedY = 0, speed_xdir = 0, speed_ydir = 0, moveAngle = 0, rotation = 0;
+Drive bot(motorFR, motorBR, motorBL, motorFL);
+float speedX = 0.0, speedY = 0.0, speed_xdir = 0.0, speed_ydir = 0.0, moveAngle = 0.0, rotation = 0.0;
 
 void checkFault(){
     bool faulted = false;
-    if(digitalRead(motorFL.nfault)==LOW) {
-        faulted = true;
-        // Serial.print("0 ");
-    }
-    // else Serial.print("1 ");
-    if(digitalRead(motorFR.nfault)==LOW) {
-        faulted = true;
-        // Serial.print("0 ");
-    }
-    // else Serial.print("1 ");
-    if(digitalRead(motorBL.nfault)==LOW) {
-        faulted = true;
-        // Serial.print("0 ");
-    }
-    // else Serial.print("1 ");
-    if(digitalRead(motorBR.nfault)==LOW) {
-        faulted = true;
-        // Serial.println("0 ");
-    }
-    // else Serial.println("1 ");
-    #ifdef CONT_CLR
-    float curTime = millis();
-    if(curTime - lastFault >= 1000){
-        motor_driver.readRegister(0b01000001);
-        lastFault = millis();
-    }
-    #else
+    if(digitalRead(motorFL.nfault)==LOW) faulted = true;
+    if(digitalRead(motorFR.nfault)==LOW) faulted = true;
+    if(digitalRead(motorBL.nfault)==LOW) faulted = true;
+    if(digitalRead(motorBR.nfault)==LOW) faulted = true;
     if(faulted){
-        Serial.println("faulted");
+        // Serial.println("faulted");
         float curTime = millis();
         if(curTime - lastFault >= 500){
             motor_driver.readRegister(0b01000001);
             lastFault = millis();
         }
     } 
-    #endif
+}
+
+void receive(int num_bytes){
+    if(num_bytes != DATA_LEN){
+        Serial.print("Received bad data length");
+        return;
+    }
+    else Serial.print("Received: ");
+    for (int i=0; i<DATA_LEN; i++){
+        if(Wire.available()){
+            buffer[i] = Wire.read();
+        }
+    }
+    buffer[DATA_LEN] = '\0';
+    if(buffer[0]!=5 && buffer[0]!=0) {
+        Serial.print("Received bad data");
+        return;
+    }
+    if(buffer[0]==0){
+        Serial.println("Bot off");
+        speedX = 0;
+        speedY = 0;
+        speed_xdir = 0;
+        speed_ydir = 0;
+        rotation = 0;
+        // bot.setDrive(0, 0, 0);
+        return;
+    }
+    Serial.println("Received motor data");
+    bool speed_x_sign = buffer[1]==1 ? true : false;
+    bool speed_y_sign = buffer[3]==1 ? true : false;
+    bool rotationsign = buffer[5]==1 ? true : false;
+    speed_xdir = (float)(buffer[2]) / 255;
+    if(!speed_x_sign) speed_xdir *= -1;
+    speed_ydir = (float)(buffer[4]) / 255;
+    if(!speed_y_sign) speed_ydir *= -1;
+    rotation = (float)(buffer[6]) / 255;
+    if(!rotationsign) rotation *= -1;
+
+    // DEBUG(speed_xdir);
+    // DEBUG(speed_ydir);
+
+    // speedX = speed_xdir * cosf(RAD(135)) + speed_ydir * cosf(RAD(45));
+    // speedY = speed_xdir * sinf(RAD(135)) + speed_ydir * sinf(RAD(45));
+    // bot.setDrive(speedX, speedY, rotation);
+    // bot.setDrive(speed_xdir, speed_ydir, rotation);
 }
 
 void setup(){
     Serial.begin(115200);
 
-    espUART.init();
+    Wire.setSDA(SDA_PIN);
+    Wire.setSCL(SCL_PIN);
+    Wire.begin(ADDR);
+    Wire.onReceive(receive);
     
     motor_driver.init();
     motor_driver.setMode();
-    analogWriteFreq(25000);
 
     strip.begin();
     strip.setBrightness(brightness);
@@ -115,6 +130,9 @@ void setup(){
 }
 
 void loop(){
+    // Serial.println("running");
+    // strip.setPixelColor(0, strip.Color(0, 0, 15));
+    // strip.show();
     if(millis() - lastLED >= BLINK_TIME){
         led_state = !led_state;
         lastLED = millis();
@@ -128,26 +146,6 @@ void loop(){
         strip.show();
     }
     checkFault();
-    bool status = espUART.uartRead((byte)5);
-    if(status){
-        // Serial.println("received data");
-        bool speed_x_sign = buffer[1]==1 ? true : false;
-        bool speed_y_sign = buffer[3]==1 ? true : false;
-        bool rotationsign = buffer[5]==1 ? true : false;
-        speed_xdir = buffer[2];
-        if(!speed_x_sign) speed_xdir *= -1;
-        speed_ydir = buffer[4];
-        if(!speed_y_sign) speed_ydir *= -1;
-        rotation = buffer[6];
-        if(!rotationsign) rotation *= -1;
-
-        // speedX = speed_xdir * cosf(RAD(135)) + speed_ydir * cosf(RAD(45));
-        // speedY = speed_xdir * sinf(RAD(135)) + speed_ydir * sinf(RAD(45));
-    }
-    // bot.setDrive(speedX, speedY, rotation);
     bot.setDrive(speed_xdir, speed_ydir, rotation);
-
-    // DEBUG(speed_xdir);
-    // DEBUG(speed_ydir);
-    // DEBUG(rotation);
 }
+
